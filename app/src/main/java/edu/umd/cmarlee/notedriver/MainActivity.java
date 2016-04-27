d9574ff1456d9b4979a046c7ac03beca4b543156
xongex <mkbrown94@gmail.com>
2016-04-27 17:33:04 -0400

    test

--- app/src/main/java/edu/umd/cmarlee/notedriver/MainActivity.java
+++ app/src/main/java/edu/umd/cmarlee/notedriver/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
+    //this is a change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
