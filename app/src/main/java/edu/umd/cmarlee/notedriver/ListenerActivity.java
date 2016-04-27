0bd58bdeace9e6367001580e0ee24e1ddfe7e195
Connor M Lee <ronnoceel@gmail.com>
2016-04-27 18:27:30 -0400

    Added NoteListActivity and changed the layout of the ListenerActivity

--- app/src/main/java/edu/umd/cmarlee/notedriver/ListenerActivity.java
+++ app/src/main/java/edu/umd/cmarlee/notedriver/ListenerActivity.java
@@ -3,26 +3,24 @@ package edu.umd.cmarlee.notedriver;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class ListenerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listener_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Intent intent = new Intent(this, NodeAdapter.class);
             }
         });
     }
---
+++ app/src/main/java/edu/umd/cmarlee/notedriver/NoteListActivity.java
@@ -0,0 +1,10 @@
+package edu.umd.cmarlee.notedriver;
+
+import android.app.Activity;
+
+/**
+ * Created by connor on 4/27/16.
+ */
+public class NoteListActivity extends Activity {
+
+}
