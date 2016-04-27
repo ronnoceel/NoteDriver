4283dabb44cb21a3801cae555ec09ef4d02c0566
Yvonne <yluk@umd.edu>
2016-04-27 17:40:43 -0400

    this is a commit comment test something blah

--- app/src/main/java/edu/umd/cmarlee/notedriver/MainActivity.java
+++ app/src/main/java/edu/umd/cmarlee/notedriver/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
     //this is a change
+    //this is also a change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
