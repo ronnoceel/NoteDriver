b471b1b0b7fb839b128137001c775a2067ad6373
connor <ronnoceel@gmail.com>
2016-04-27 20:49:32 -0400

    Implemented NoteListActivity slightly

--- app/src/main/java/edu/umd/cmarlee/notedriver/NoteListActivity.java
+++ app/src/main/java/edu/umd/cmarlee/notedriver/NoteListActivity.java
@@ -1,10 +1,176 @@
 package edu.umd.cmarlee.notedriver;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-/**
- * Created by connor on 4/27/16.
- */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.Date;
+
+
 public class NoteListActivity extends ListActivity {
 
+    private static final int ADD_TODO_ITEM_REQUEST = 0;
+    private static final String FILE_NAME = "NotesList.txt";
+    private static final String TAG = "NoteDriver";
+
+    private static final int ADD_REQUEST_CODE = 1;
+
+    // IDs for menu items
+    private static final int MENU_DELETE = Menu.FIRST;
+    private static final int MENU_DUMP = Menu.FIRST + 1;
+
+    NoteAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAdapter = new NoteAdapter(getApplicationContext());
+
+        getListView().setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "Entered onActivityResult()");
+
+        // TODO - Check result code and request code
+        // if user submitted a new ToDoItem
+        // Create a new ToDoItem from the data Intent
+        // and then add it to the adapter
+        switch (resultCode) {
+            case RESULT_CANCELED:
+                Log.i(TAG, "Returned after canceling");
+                break;
+            case RESULT_OK:
+                Note note = new Note(data);
+                mAdapter.add(note);
+                break;
+        }
+    }
+
+    // Do not modify below here
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Load saved ToDoItems, if necessary
+
+        if (mAdapter.getCount() == 0)
+            loadItems();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Save ToDoItems
+
+        saveItems();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all");
+        menu.add(Menu.NONE, MENU_DUMP, Menu.NONE, "Dump to log");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_DELETE:
+                mAdapter.clear();
+                return true;
+            case MENU_DUMP:
+                dump();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void dump() {
+
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            String data = ((Note) mAdapter.getItem(i)).toLog();
+            Log.i(TAG,	"Item " + i + ": " + data.replace(Note.ITEM_SEP, ","));
+        }
+
+    }
+
+    // Load stored ToDoItems
+    //TODO: REDO this method! It will only read one line at a time for each field of the Note object.
+    private void loadItems() {
+        BufferedReader reader = null;
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            reader = new BufferedReader(new InputStreamReader(fis));
+
+            String text = null;
+            String subject = null;
+            Date date = null;
+
+            while (null != (text = reader.readLine())) {
+                subject = reader.readLine();
+                date = Note.FORMAT.parse(reader.readLine());
+                mAdapter.add(new Note(text, subject, date));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // Save ToDoItems to file
+    private void saveItems() {
+        PrintWriter writer = null;
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    fos)));
+
+            for (int idx = 0; idx < mAdapter.getCount(); idx++) {
+
+                writer.println(mAdapter.getItem(idx));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != writer) {
+                writer.close();
+            }
+        }
+    }
 }
