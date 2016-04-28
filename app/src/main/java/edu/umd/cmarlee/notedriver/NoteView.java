8d94388c1780cccb97ba3063793a053256b42e4c
xongex <mkbrown94@gmail.com>
2016-04-28 16:25:03 -0400

    added note view activity and layout

---
+++ app/src/main/java/edu/umd/cmarlee/notedriver/NoteView.java
@@ -0,0 +1,21 @@
+package edu.umd.cmarlee.notedriver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mbrown on 4/28/2016.
+ */
+public class NoteView extends Activity {
+
+
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_view);
+
+
+    }
+    
+
+
+}
