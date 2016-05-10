a7e57a9dbb64a891a194f3a3f91ce5236dfa7de5
connor <ronnoceel@gmail.com>
2016-05-10 17:42:40 -0400

    Removed the keys from Github... unsafe to leave those available to anyone.

--- app/src/main/java/edu/umd/cmarlee/notedriver/Constants.java
+++ app/src/main/java/edu/umd/cmarlee/notedriver/Constants.java
@@ -4,20 +4,9 @@ package edu.umd.cmarlee.notedriver;
  * General constants.
  */
 public class Constants {
-    //public static final String CLIENT_ID = "53DOYPQxtsZwiX3tXyojhA==";
-    //public static final String CLIENT_KEY = "CSHblg_C9EujX3epaeoTmu6T-UvsAoxFUOBgEfERJRUS2qUQXMrxhJNEW5IcVQMtvErRkU65yQMKnXpnLX4WQQ==";
 
-    //public static final String CLIENT_ID = "tFdXApu32XuFnJD-DBxT_Q==";
-    //public static final String CLIENT_KEY = "mZUXftlzPKJGc0a1e20aDr0-P-RTtRrN1RfOgFybkEnNKWcLlvGg4sVXxH2bYe9Oi5Rcw4lYn_OepyrlERl9PQ==";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_KEY = "";
 
-    //public static final String CLIENT_ID = "tI4BZnIbvWCPwcd7JK9EzQ==";
-    //public static final String CLIENT_KEY = "EP-X-hLgbDX2gi0a6dhl78J5_igDjS5ALhAZwn19zl4g0Ct5STLrAZqoZ1Esuz_w6S-B6QOTDfvhmTLTt3vXSw==";
-
-    //Connor's Keys
-    //public static final String CLIENT_ID = "hcEzW_Xq7OycLUQn_9gDjg==";
-    //public static final String CLIENT_KEY = "_UNwm5AlQY61L1LEtds7XGDnkk28xXABZh9MOHvXglddz7yLs3aJzUEFyB0U1V7Se3U5Lb8aJYAfNuTKmyiHJw==";
-
-    public static final String CLIENT_ID = "3VS1mNomrQhpQF_WT5ouaQ==";
-    public static final String CLIENT_KEY = "OwjvIdzatCTXsRFAw5D0v-T4mHFg72KPvyjcVr2Bw6Hz5iebJeYrlyXXDji90doTnDj_WCXypoE8HK_ImdXpGw==";
     public static final String FILE_NAME = "NotesList.txt";
 }
