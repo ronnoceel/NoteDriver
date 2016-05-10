7859d5081c84dbab3e243fb22eba13b74bffc26a
Yvonne <yluk@umd.edu>
2016-05-09 20:14:45 -0400

    Added Constants.java

---
+++ app/src/main/java/edu/umd/cmarlee/notedriver/Constants.java
@@ -0,0 +1,16 @@
+package edu.umd.cmarlee.notedriver;
+
+/**
+ * General constants.
+ */
+public class Constants {
+    //public static final String CLIENT_ID = "53DOYPQxtsZwiX3tXyojhA==";
+    //public static final String CLIENT_KEY = "CSHblg_C9EujX3epaeoTmu6T-UvsAoxFUOBgEfERJRUS2qUQXMrxhJNEW5IcVQMtvErRkU65yQMKnXpnLX4WQQ==";
+
+    //public static final String CLIENT_ID = "tFdXApu32XuFnJD-DBxT_Q==";
+    //public static final String CLIENT_KEY = "mZUXftlzPKJGc0a1e20aDr0-P-RTtRrN1RfOgFybkEnNKWcLlvGg4sVXxH2bYe9Oi5Rcw4lYn_OepyrlERl9PQ==";
+
+    public static final String CLIENT_ID = "tI4BZnIbvWCPwcd7JK9EzQ==";
+    public static final String CLIENT_KEY = "EP-X-hLgbDX2gi0a6dhl78J5_igDjS5ALhAZwn19zl4g0Ct5STLrAZqoZ1Esuz_w6S-B6QOTDfvhmTLTt3vXSw==";
+    public static final String FILE_NAME = "NotesList.txt";
+}
